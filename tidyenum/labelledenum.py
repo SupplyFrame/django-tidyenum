4dd261ac75af1fdf8f8290d107caf4d511bd6425
Christopher Lambacher <chris@kateandchris.net>
2013-09-13 11:11:17 -0400

    Doc strings

--- tidyenum/labelledenum.py
+++ tidyenum/labelledenum.py
@@ -10,6 +10,21 @@ class classproperty(object):
 
 
 class LabelledEnum(Enum):
+    """
+    Base class that allows for the creation of an enum with a value and a label
+    using tuples as the Enum values. Using the LabelledIntEnum for example:
+        >>> from labelledenum import LabelledIntEnum
+        >>> class MyChoices(LabelledIntEnum):
+        ...     one = (1, 'The First Choice')
+        ...     two = (2, 'The Second Choice')
+        ...
+        >>> MyChoices.one
+        <MyChoices.one: 1>
+        >>> MyChoices.one.label
+        'The First Choice'
+        >>> MyChoices.one.value
+        1
+    """
     def __new__(cls, *args):
         if not hasattr(cls, '_member_type_'):
             cls._member_type_, first_enum = cls.__class__._get_mixins_(cls.__bases__)
@@ -27,8 +42,36 @@ class LabelledEnum(Enum):
 
 
 class LabelledIntEnum(int, LabelledEnum):
-    pass
+    """
+    Base class that allows for the creation of an enum with an int value and a label
+    using tuples as the Enum values. Example:
+        >>> from labelledenum import LabelledIntEnum
+        >>> class MyChoices(LabelledIntEnum):
+        ...     one = (1, 'The First Choice')
+        ...     two = (2, 'The Second Choice')
+        ...
+        >>> MyChoices.one
+        <MyChoices.one: 1>
+        >>> MyChoices.one.label
+        'The First Choice'
+        >>> MyChoices.one.value
+        1
+    """
 
 
 class LabelledUnicodeEnum(unicode, LabelledEnum):
-    pass
+    """
+    Base class that allows for the creation of an enum with a unicode value and a label
+    using tuples as the Enum values. Example:
+        >>> from labelledenum import LabelledUnicodeEnum
+        >>> class MyChoices(LabelledUnicodeEnum):
+        ...     one = ('one', 'The First Choice')
+        ...     two = ('two', 'The Second Choice')
+        ...
+        >>> MyChoices.one
+        <MyChoices.one: 'one'>
+        >>> MyChoices.one.label
+        'The First Choice'
+        >>> MyChoices.one.value
+        'one'
+    """
